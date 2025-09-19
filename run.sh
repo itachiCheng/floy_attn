952db33c0b0652f92e50d622cf5f910cc2b152d5
itachiCheng <648085247@qq.com>
2025-09-19 19:19:00 +0800

    update

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 rm -rf build/
-rm -rf examples src
-scp -r  root@121.40.67.29:/home/cq/foly_attn_proj/examples ./
-scp -r  root@121.40.67.29:/home/cq/foly_attn_proj/src ./
+#rm -rf examples src
+#scp -r  root@121.40.67.29:/home/cq/foly_attn_proj/examples ./
+#scp -r  root@121.40.67.29:/home/cq/foly_attn_proj/src ./
 
 
 mkdir build && cd build
@@ -9,7 +9,7 @@ cmake ..
 make package -j 144
 
 ./CANN-custom_ops-8.2.0.0-linux.aarch64.run --quiet
-cmake .. -DTESTS_EXAMPLE_OPS_TEST=test_flash_attention_score
+cmake .. -DTESTS_EXAMPLE_OPS_TEST=test_fused_floyd_attention
 make
 
 
