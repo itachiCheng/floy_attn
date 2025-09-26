04026639dd97c3c93d3bfc88fb6638b125d239b7
c00611613 <chengqiang10@huawei.com>
2025-09-26 21:18:50 +0800

    update floyd

--- examples/transformer/scripts/fa_print_result.py
+++ examples/transformer/scripts/fa_print_result.py
@@ -38,9 +38,9 @@ def show():
         # print("attention_out's shape: ", attention_out.shape)
         attention_out_raw = np.fromfile('attn.bin', dtype=np.float16)
         print('attn result: ', attention_out_raw)
-        x_max = np.fromfile('x_max.bin', dtype=np.float16)
+        x_max = np.fromfile('x_max.bin', dtype=np.float32)
         print('x_max result: ', x_max)
-        x_sum = np.fromfile('x_sum.bin', dtype=np.float16)
+        x_sum = np.fromfile('x_sum.bin', dtype=np.float32)
         print('x_sum result: ', x_sum)
         absolute_errors = np.abs(attention_out - attention_out_raw)
 
