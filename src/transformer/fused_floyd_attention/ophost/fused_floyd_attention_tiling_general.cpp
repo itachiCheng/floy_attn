2d73cf04827b2373bdc7c6d99d6db97e079675e4
c00611613 <chengqiang10@huawei.com>
2025-09-26 21:51:25 +0800

    update floyd

--- src/transformer/fused_floyd_attention/ophost/fused_floyd_attention_tiling_general.cpp
+++ src/transformer/fused_floyd_attention/ophost/fused_floyd_attention_tiling_general.cpp
@@ -1707,8 +1707,8 @@ protected:
         //     }
         // }
         // return true;
-        bmm1.SetShape(1, 1024, 32); // 512 512 32
-        bmm1.SetOrgShape(1024, 1024, 32);
+        bmm1.SetShape(1024, 1024, 32); // 512 512 32
+        bmm1.SetOrgShape(1, 1024, 32);
         bmm1.SetALayout(1, 1, 1, 128, 32); // BSNGD
         bmm1.SetBLayout(1, 1024, 1, 128, 32); // S BNG D  128 16 128
         bmm1.SetCLayout(1, 1, 128, 1, 1024);
