c10dfe4d9d7384ea8f3f513a5081e51295b7ee09
c00611613 <chengqiang10@huawei.com>
2025-10-13 15:38:39 +0800

    update

--- examples/transformer/scripts/fa_generate_data.py
+++ examples/transformer/scripts/fa_generate_data.py
@@ -114,7 +114,7 @@ if case_name == 'test_fused_floyd_attention':
     v = torch.from_numpy(value).to(device)
     mask_npu = torch.from_numpy(mask).to(device)  # float32
 
-    logits = torch.einsum('bhikc,bhijc->bhikj', q, k)#torch.einsum('bhikc,bhjkc->bhikj', q, k1)#torch.einsum('bhikc,bhijc->bhikj', q, k)*scale
+    logits = torch.einsum('bhikc,bhjkc->bhikj', q, k1)#torch.einsum('bhikc,bhijc->bhikj', q, k)##torch.einsum('bhikc,bhijc->bhikj', q, k)*scale
 
     attention_mask = 1e12 * (mask_npu - 1)                 # (B,1,S,S)
     logits = logits + attention_mask.half()                  # 按旧脚本用 float16 加
