65de02afd4367c5bb4515cd99ab5616758c97f1c
c00611613 <chengqiang10@huawei.com>
2025-10-13 22:07:03 +0800

    update

--- run.sh
+++ run.sh
@@ -10,8 +10,8 @@ rm -rf build/
 #scp -r  root@121.40.67.29:/home/cq/foly_attn_proj/examples ./
 #scp -r  root@121.40.67.29:/home/cq/foly_attn_proj/src ./
 
-export ASCEND_SLOG_PRINT_TO_STDOUT=1
-export ASCEND_GLOBAL_LOG_LEVEL=0
+#export ASCEND_SLOG_PRINT_TO_STDOUT=1
+#export ASCEND_GLOBAL_LOG_LEVEL=0
 mkdir build && cd build
 cmake ..
 make package -j 144
