7ab2f4cf7f6a00df46eb1222105b876a974a37f0
c00611613 <chengqiang10@huawei.com>
2025-10-13 10:43:49 +0800

    update

--- examples/transformer/scripts/fa_generate_data.py
+++ examples/transformer/scripts/fa_generate_data.py
@@ -92,65 +92,65 @@ if case_name == 'test_fused_floyd_attention':
     device = torch.device('npu')
 
     #######################################################生成数据+小算子拼接###############################################################
-    # query = np.random.uniform(2, 5, (B, H, N, M, D)).astype(dtype)
-    # key = np.random.uniform(2, 5, (B, H, N, K, D)).astype(dtype)
-    # value = np.random.uniform(2, 5, (B, H, N, K, D)).astype(dtype)
-    # key1 = np.random.uniform(2, 5, (B, H, K, M, D)).astype(dtype)
-    # value1 = np.random.uniform(2, 5, (B, H, K, M, D)).astype(dtype)
-    # mask = np.concatenate((np.ones((B, H, N, M, K - K//2)).astype(np.float32), np.zeros((B, H, N, M, K//2)).astype(np.float32)), axis=-1)
+    query = np.random.uniform(2, 5, (B, H, N, M, D)).astype(dtype)
+    key = np.random.uniform(2, 5, (B, H, N, K, D)).astype(dtype)
+    value = np.random.uniform(2, 5, (B, H, N, K, D)).astype(dtype)
+    key1 = np.random.uniform(2, 5, (B, H, K, M, D)).astype(dtype)
+    value1 = np.random.uniform(2, 5, (B, H, K, M, D)).astype(dtype)
+    mask = np.concatenate((np.ones((B, H, N, M, K - K//2)).astype(np.float32), np.zeros((B, H, N, M, K//2)).astype(np.float32)), axis=-1)
 
-    # evo_mask = 1 - mask.astype(np.uint8)
-    # query.tofile(os.path.join(os.path.dirname(__file__), "query.bin"))
-    # key.tofile(os.path.join(os.path.dirname(__file__), "key.bin"))
-    # value.tofile(os.path.join(os.path.dirname(__file__), 'value.bin'))
-    # key1.tofile(os.path.join(os.path.dirname(__file__), 'key1.bin'))
-    # value1.tofile(os.path.join(os.path.dirname(__file__), 'value1.bin'))
-    # evo_mask.tofile(os.path.join(os.path.dirname(__file__), 'atten_mask.bin'))
-    # device = torch.device('npu')
+    evo_mask = 1 - mask.astype(np.uint8)
+    query.tofile(os.path.join(os.path.dirname(__file__), "query.bin"))
+    key.tofile(os.path.join(os.path.dirname(__file__), "key.bin"))
+    value.tofile(os.path.join(os.path.dirname(__file__), 'value.bin'))
+    key1.tofile(os.path.join(os.path.dirname(__file__), 'key1.bin'))
+    value1.tofile(os.path.join(os.path.dirname(__file__), 'value1.bin'))
+    evo_mask.tofile(os.path.join(os.path.dirname(__file__), 'atten_mask.bin'))
+    device = torch.device('npu')
 
-    # q = torch.from_numpy(query).to(device)      # float16 -> NPU
-    # k = torch.from_numpy(key).to(device)
-    # k1 = torch.from_numpy(key1).to(device)
-    # v = torch.from_numpy(value).to(device)
-    # mask_npu = torch.from_numpy(mask).to(device)  # float32
-
-    # logits = torch.einsum('bhikc,bhijc->bhikj', q, k)#torch.einsum('bhikc,bhjkc->bhikj', q, k1)#torch.einsum('bhikc,bhijc->bhikj', q, k)*scale
-
-    # attention_mask = 1e12 * (mask_npu - 1)                 # (B,1,S,S)
-    # logits = logits + attention_mask.half()                  # 按旧脚本用 float16 加
-
-    # weight, x_max, x_sum = tsoftmax(logits.float())
-
-    # attn = torch.einsum('bhikj,bhijc->bhikc', weight, v)
-
-    # attn.cpu().numpy().tofile('attn.bin')
-    # x_max.cpu().numpy().tofile('x_max.bin')
-    # x_sum.cpu().numpy().tofile('x_sum.bin')
-    #######################################################################################################################################
-
-
-    query = np.fromfile(os.path.join(os.path.dirname(__file__), "query.bin"), dtype=np.float16, ).reshape(B, H, N, M, D)
-    key = np.fromfile(os.path.join(os.path.dirname(__file__), "key.bin"), dtype=np.float16).reshape(B, H, N, K, D)
-    value = np.fromfile(os.path.join(os.path.dirname(__file__), "value.bin"), dtype=np.float16).reshape(B, H, N, K, D)
-    key1 = np.fromfile(os.path.join(os.path.dirname(__file__), "key1.bin"), dtype=np.float16).reshape(B, H, K, M, D)
-    value1 = np.fromfile(os.path.join(os.path.dirname(__file__), "value1.bin"), dtype=np.float16).reshape(B, H, K, M, D)
-    
-    mask = 1 - np.fromfile(os.path.join(os.path.dirname(__file__), "atten_mask.bin"), dtype=np.uint8).reshape(B, H, N, M, K)
-    mask = mask.astype(np.float32)
     q = torch.from_numpy(query).to(device)      # float16 -> NPU
     k = torch.from_numpy(key).to(device)
-    v = torch.from_numpy(value).to(device)
     k1 = torch.from_numpy(key1).to(device)
-    v1 = torch.from_numpy(value1).to(device)
+    v = torch.from_numpy(value).to(device)
     mask_npu = torch.from_numpy(mask).to(device)  # float32
-    logits =  torch.einsum('bhikc,bhijc->bhikj', q, k)#torch.einsum('bhikc,bhjkc->bhikj', q, k1)#  torch.einsum('bhikc,bhijc->bhikj', q, k)# + 
-    attention_mask = 1e9 * (mask_npu - 1)                 # (B,1,S,S)
+
+    logits = torch.einsum('bhikc,bhijc->bhikj', q, k)#torch.einsum('bhikc,bhjkc->bhikj', q, k1)#torch.einsum('bhikc,bhijc->bhikj', q, k)*scale
+
+    attention_mask = 1e12 * (mask_npu - 1)                 # (B,1,S,S)
     logits = logits + attention_mask.half()                  # 按旧脚本用 float16 加
+
     weight, x_max, x_sum = tsoftmax(logits.float())
 
-    attn = torch.einsum('bhikj,bhijc->bhikc', weight, v)# + torch.einsum('bhikj,bhijc->bhikc', weight, v1)
+    attn = torch.einsum('bhikj,bhijc->bhikc', weight, v)
+
     attn.cpu().numpy().tofile('attn.bin')
     x_max.cpu().numpy().tofile('x_max.bin')
     x_sum.cpu().numpy().tofile('x_sum.bin')
+    #######################################################################################################################################
+
+
+    # query = np.fromfile(os.path.join(os.path.dirname(__file__), "query.bin"), dtype=np.float16, ).reshape(B, H, N, M, D)
+    # key = np.fromfile(os.path.join(os.path.dirname(__file__), "key.bin"), dtype=np.float16).reshape(B, H, N, K, D)
+    # value = np.fromfile(os.path.join(os.path.dirname(__file__), "value.bin"), dtype=np.float16).reshape(B, H, N, K, D)
+    # key1 = np.fromfile(os.path.join(os.path.dirname(__file__), "key1.bin"), dtype=np.float16).reshape(B, H, K, M, D)
+    # value1 = np.fromfile(os.path.join(os.path.dirname(__file__), "value1.bin"), dtype=np.float16).reshape(B, H, K, M, D)
+    
+    # mask = 1 - np.fromfile(os.path.join(os.path.dirname(__file__), "atten_mask.bin"), dtype=np.uint8).reshape(B, H, N, M, K)
+    # mask = mask.astype(np.float32)
+    # q = torch.from_numpy(query).to(device)      # float16 -> NPU
+    # k = torch.from_numpy(key).to(device)
+    # v = torch.from_numpy(value).to(device)
+    # k1 = torch.from_numpy(key1).to(device)
+    # v1 = torch.from_numpy(value1).to(device)
+    # mask_npu = torch.from_numpy(mask).to(device)  # float32
+    # logits =  torch.einsum('bhikc,bhijc->bhikj', q, k)#torch.einsum('bhikc,bhjkc->bhikj', q, k1)#  torch.einsum('bhikc,bhijc->bhikj', q, k)# + 
+    # attention_mask = 1e9 * (mask_npu - 1)                 # (B,1,S,S)
+    # logits = logits + attention_mask.half()                  # 按旧脚本用 float16 加
+    # weight, x_max, x_sum = tsoftmax(logits.float())
+
+    # attn = torch.einsum('bhikj,bhijc->bhikc', weight, v)# + torch.einsum('bhikj,bhijc->bhikc', weight, v1)
+    # attn.cpu().numpy().tofile('attn.bin')
+    # x_max.cpu().numpy().tofile('x_max.bin')
+    # x_sum.cpu().numpy().tofile('x_sum.bin')
 else:
     raise RuntimeError(f"Invalid case name:", case_name)
